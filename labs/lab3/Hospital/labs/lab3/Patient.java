13db55dd2fff57cb68c38a432302d912e920c316
Zacharias Göransson <zacharias.goransson@student.kyh.se>
2020-09-22 16:47:08 +0200

    Initial commit

---
+++ labs/lab3/Hospital/labs/lab3/Patient.java
@@ -0,0 +1,43 @@
+package Hospital.labs.lab3;
+
+
+// 4 medlemsmetoder
+// getName, isSick, takeMedication, getSickness
+// 2 konstruktorer
+// Name, Sickness
+
+public class Patient {
+
+    private String name;
+    private String sickness;
+    public  Patient(String name, String sickness) { //Patienter förutom Helena från array
+        this.name = name; //probs fel
+        this.sickness = sickness; //probs fel
+    }
+
+    public  Patient(String Name) { //Helena från array
+ // HELENA???
+    }
+
+
+    public String getName(){
+
+    }
+
+
+    public String isSick(){
+
+    }
+
+
+    public String takeMedication(){
+
+    }
+
+
+    public String getSickness(){
+
+    }
+
+
+}

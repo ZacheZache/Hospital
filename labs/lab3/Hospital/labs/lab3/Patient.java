83c2fffd3ba20a8ecd033555ba318511143d3697
Zacharias Göransson <zacharias.goransson@student.kyh.se>
2020-09-24 16:38:21 +0200

    Changed the method isSick, added the method takeMedication, and added comments.

--- labs/lab3/Hospital/labs/lab3/Patient.java
+++ labs/lab3/Hospital/labs/lab3/Patient.java
@@ -7,38 +7,45 @@ package Hospital.labs.lab3;
 // Name, Sickness
 
 public class Patient {
+    //Klass som förklarar pacienternas egenskaper
 
     private String name;
     private String sickness;
 
-    public  Patient(String name, String sickness) { //Patienter förutom Helena från array
+    public Patient(String name, String sickness) {
+        //Kopplar till arrayen Patient
         this.name = name;
         this.sickness = sickness;
     }
 
-    public  Patient(String Name) { //Helena från array
-        // HELENA???
+    public Patient(String name) {
+        //Överskrider den första konstruktorn om "sickness" saknas
+        this.name = name;
     }
 
 
-    public String getName(){
-        return  name;
+    public String getName() {
+        //hämtar namn
+        return name;
     }
 
-    public String getSickness(){
+    public String getSickness() {
+        //hämtar sjukdom
         return sickness;
     }
 
 
-
-    public boolean isSick(){
-        return !this.sickness.isEmpty();
-
+    public boolean isSick() {
+        //Om pacienten saknar sjukdom så sätter vi värdet på "sickness" null
+        return sickness != null;
     }
 
+    public String takeMedication(Medicine medicine) {
+        //Vi sätter sickness till null för att medicinen matchar sjukdomen
+        if (medicine.getTreatmentName().equals(sickness))
+            sickness = null;
 
-    public String takeMedication(){
-
+        return sickness;
     }
 
 }

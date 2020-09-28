0ad73a7097a5c40422f463cb08e0d412e6e8dced
Zacharias Göransson <zacharias.goransson@student.kyh.se>
2020-09-28 17:19:30 +0200

    Changed the methods accessibility from public to default

--- labs/lab3/Hospital/labs/lab3/Patient.java
+++ labs/lab3/Hospital/labs/lab3/Patient.java
@@ -12,35 +12,35 @@ public class Patient {
     private String name;
     private String sickness;
 
-    public Patient(String name, String sickness) {
+    Patient(String name, String sickness) {
         //Kopplar till arrayen Patient
         this.name = name;
         this.sickness = sickness;
     }
 
-    public Patient(String name) {
+    Patient(String name) {
         //Överskrider den första konstruktorn om "sickness" saknas
         this.name = name;
     }
 
 
-    public String getName() {
+    String getName() {
         //hämtar namn
         return name;
     }
 
-    public String getSickness() {
+    String getSickness() {
         //hämtar sjukdom
         return sickness;
     }
 
 
-    public boolean isSick() {
+    boolean isSick() {
         //Om pacienten saknar sjukdom så sätter vi värdet på "sickness" null
         return sickness != null;
     }
 
-    public String takeMedication(Medicine medicine) {
+    String takeMedication(Medicine medicine) {
         //Vi sätter sickness till null för att medicinen matchar sjukdomen
         if (medicine.getTreatmentName().equals(sickness))
             sickness = null;
